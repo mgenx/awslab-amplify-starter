2fba42252c336c21eed226fd454edb3a2cc20482
mgenx <krexspace@gmail.com>
2022-12-28 10:15:19 -0800

    test change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          One great React app at a time.
         </p>
         <a
           className="App-link"
